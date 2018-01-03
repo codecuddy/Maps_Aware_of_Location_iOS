a2a2ff0dcbb93da2c48f0361a50037c1dcb9fb3e
Charlie <charliecuddy4@gmail.com>
2018-01-03 14:18:12 -0600

    identify current location with reverseGeocodeLocation

--- Location_Aware/ViewController.swift
+++ Location_Aware/ViewController.swift
@@ -32,22 +32,90 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
     }
 
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+
         let userLocation: CLLocation = locations[0]
-        
-        let latitude = userLocation.coordinate.latitude
-        
-        let longitude = userLocation.coordinate.longitude
-        
-        let latDelta: CLLocationDegrees = 0.05
-        let lonDelta: CLLocationDegrees = 0.05
-        
-        let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta)
-        
-        let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
-        
-        let region = MKCoordinateRegion(center: location, span: span)
-        
-        self.map.setRegion(region, animated: true)
+
+        CLGeocoder().reverseGeocodeLocation(userLocation) { (placemarks, error) in
+            
+            if error != nil {
+                
+                print(error)
+                
+            } else {
+                
+                if let placemark = placemarks?[0] {
+                    
+                    //subThoroughfare --> the number address
+                    var subThoroughfare = ""
+                    
+                    if placemark.subThoroughfare != nil {
+                        
+                        subThoroughfare = placemark.subThoroughfare!
+                        
+                    }
+                    
+                    //thoroughfare --> the street name
+                    var thoroughfare = ""
+                    
+                    if placemark.thoroughfare != nil {
+                        
+                        thoroughfare = placemark.thoroughfare!
+                    }
+                    
+                    // locality --> the city
+                    var locality = ""
+                    
+                    if placemark.locality != nil {
+                        
+                        locality = placemark.locality!
+                    }
+
+                    // subLocality --> the 'neighborhood' ??
+                    var subLocality = ""
+                    
+                    if placemark.subLocality != nil {
+                        
+                        subLocality = placemark.subLocality!
+                    }
+                    
+                    // subAdministritiveArea --> the county
+                    var subAdministritiveArea = ""
+                    
+                    if placemark.subAdministrativeArea != nil {
+                        
+                        subAdministritiveArea = placemark.subAdministrativeArea!
+                    }
+                    
+                    // administritiveArea --> the state
+                    var administritiveArea = ""
+                    
+                    if placemark.administrativeArea != nil {
+                        
+                        administritiveArea = placemark.administrativeArea!
+                    }
+                    
+                    // postalCode --> the area code
+                    var postalCode = ""
+                    
+                    if placemark.postalCode != nil {
+                        
+                        postalCode = placemark.postalCode!
+                    }
+
+                    // country --> the country
+                    var country = ""
+                    
+                    if placemark.country != nil {
+                        
+                        country = placemark.country!
+                    }
+                    
+                    print(subThoroughfare + " " + thoroughfare + " (Neighborhood: \(subLocality))" + "\n" + locality + " (County: \(subAdministritiveArea))" + "\n" + administritiveArea + " " + postalCode + "\n" + country)
+                }
+                
+            }
+            
+        }
     }
     
 }
